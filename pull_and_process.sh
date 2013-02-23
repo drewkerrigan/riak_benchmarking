aec69f24f07c68ec8852b88932f48b03ddc5ea37
drewkerrigan <dkerrigan@basho.com>
2013-02-23 01:01:49 -0500

    removing summarize results and adding more results

--- pull_and_process.sh
+++ pull_and_process.sh
@@ -1,5 +1,2 @@
 #! /bin/bash
-
-s3cmd sync s3://basho-proserv-benchmarking/results . --exclude "*ip-*" --exclude "*preload*" --exclude "*snapshot*" --exclude "*perfmed*" --exclude "*testing*" --exclude "*slriakservers/20130208*" --exclude "*slriakservers/20130209*"   --exclude "*slriakservers/20130207_214936*" --exclude "*functionality_2i-1.2.1-eleveldb*" --exclude "*functionality_2i-1.3-eleveldb*"
-
-python SummarizeResults.py results > summary.csv
+s3cmd sync s3://basho-proserv-benchmarking/results . --exclude "*ip-*" --exclude "*preload*" --exclude "*snapshot*" --exclude "*perfmed*" --exclude "*testing*" --exclude "*slriakservers/20130208*" --exclude "*slriakservers/20130209*"   --exclude "*slriakservers/20130207_214936*" --exclude "*functionality_2i-1.2.1-eleveldb*" --exclude "*functionality_2i-1.3-eleveldb*" --exclude "*aws_benchmark/20130220-204139*"
